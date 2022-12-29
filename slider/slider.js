20bf30a1b95ab49a86aebe1ef2024693550d9a11
Андрей Барышев <baryshev_2002@inbox.ru>
2022-12-30 00:22:13 +0600

    сделал слайдер с локальной памятью

---
+++ slider/slider.js
@@ -0,0 +1,40 @@
+let slides = document.getElementsByClassName("mySlides");
+let slideIndex;
+
+if (localStorage.getItem('index') == null && JSON.parse(localStorage.getItem('index')) !== 1){
+    slideIndex = 1;
+    localStorage.setItem('index', JSON.stringify(slideIndex));
+}
+else
+{
+    slideIndex = JSON.parse(localStorage.getItem('index'));
+}
+
+//slideIndex = localStorage.getItem('index');
+function plusSlides(n){
+    slideIndex+=n;
+    localStorage.setItem('index', JSON.stringify(slideIndex));
+    showSlides(slideIndex);
+}
+
+function showSlides(n){
+    let i;
+
+    if (n > slides.length){
+        slideIndex = 1;
+        localStorage.setItem('index', JSON.stringify(slideIndex))
+    }
+
+    if (n < 1){
+        slideIndex = slides.length;
+        localStorage.setItem('index', JSON.stringify(slideIndex));
+    }
+
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+
+    console.log(slides[slideIndex-1].style.display = "block")
+    localStorage.setItem('index', JSON.stringify(slideIndex))
+    console.log(JSON.parse(localStorage.getItem('index')));
+}
