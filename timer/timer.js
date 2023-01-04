812528e0da75ffd47df543772a6f462dfc62f6b6
Андрей Барышев <baryshev_2002@inbox.ru>
2023-01-04 19:20:29 +0600

    внедрил localstorage

--- timer/timer.js
+++ timer/timer.js
@@ -1,6 +1,29 @@
-
-function myClick(){
-    console.log('work');
+/* Значение таймера 0:0 */
+if (localStorage.getItem('minuteId') == null){
+    localStorage.setItem('minuteId', JSON.stringify(0));
+    console.log("1");
 }
 
-/* Значение таймера 0:0 */
+if (localStorage.getItem('secondId') == null){
+    localStorage.setItem('secondId', JSON.stringify(0));
+    console.log("2");
+}
+
+
+
+/* Кнопки */
+function startTimer(){
+    console.log('start');
+    let element = document.getElementsByClassName('minutes')[0].value;
+    console.log(element);
+}
+
+function stopTimer(){
+    console.log('stop');
+}
+
+function resetTimer(){
+    console.log('reset');
+}
+
+
