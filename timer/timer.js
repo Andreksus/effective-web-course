733384417dbda791d9eb7773bcf4b2deead4b9c6
Андрей Барышев <baryshev_2002@inbox.ru>
2023-01-05 00:57:45 +0600

    сделал start

--- timer/timer.js
+++ timer/timer.js
@@ -1,21 +1,57 @@
 /* Значение таймера 0:0 */
-if (localStorage.getItem('minuteId') == null){
+/* Объявил интервал */
+let timer = null;
+
+/* Первая загрузка */
+if
+(localStorage.getItem('minuteId') == null || JSON.parse(localStorage.getItem('secondId')) == null) {
     localStorage.setItem('minuteId', JSON.stringify(0));
-    console.log("1");
-}
-
-if (localStorage.getItem('secondId') == null){
     localStorage.setItem('secondId', JSON.stringify(0));
-    console.log("2");
+    localStorage.setItem('condition', JSON.stringify(0));
+    console.log("Первый запуск таймера: грузим локальное хранилище")
 }
 
+if (JSON.parse(localStorage.getItem('condition')) == 1){
+    timer = setInterval(updateTimer, 1000);
+}
 
+function updateTimer(){
+    if (JSON.parse(localStorage.getItem('secondId')) <= 0 && JSON.parse(localStorage.getItem('minuteId')) <= 0 && JSON.parse(localStorage.getItem('condition')) == 1){
+        clearInterval(timer);
+        timer = null;
+        localStorage.setItem('condition', JSON.stringify(0));
+        document.body.style.backgroundColor = "#ff0000"
+        return;
+        //console.log("Не конец?")
+    };
+
+    if (JSON.parse(localStorage.getItem('secondId')) == 0 && JSON.parse(localStorage.getItem('condition')) == 1){
+        localStorage.setItem('minuteId', JSON.stringify(JSON.parse(localStorage.getItem('minuteId'))) - 1);
+        console.log("Прошла минута")
+        localStorage.setItem('secondId', JSON.stringify(60));
+    };
+    if (JSON.parse(localStorage.getItem('condition')) == 1){
+        console.log("Прошла секунда");
+        localStorage.setItem('secondId', JSON.stringify(JSON.parse(localStorage.getItem('secondId'))-1));
+        document.querySelector('.minutes').value = JSON.parse(localStorage.getItem('minuteId'));
+        document.querySelector('.seconds').value = JSON.stringify(JSON.parse(JSON.parse(localStorage.getItem('secondId'))));
+    }
+    if (JSON.parse(localStorage.getItem('condition')) == 0){
+        clearInterval();
+    }
+}
 
 /* Кнопки */
 function startTimer(){
-    console.log('start');
-    let element = document.getElementsByClassName('minutes')[0].value;
-    console.log(element);
+    if (timer != null){
+        return;
+    }
+    document.body.style.backgroundColor = "#ffffff";
+    console.log("Нажали на старт: запуск таймера");
+    localStorage.setItem('minuteId', document.querySelector('.minutes').value);
+    localStorage.setItem('secondId', document.querySelector('.seconds').value);
+    localStorage.setItem('condition', JSON.stringify(1));
+    timer = setInterval(updateTimer, 1000);
 }
 
 function stopTimer(){
@@ -27,3 +63,42 @@ function resetTimer(){
 }
 
 
+
+// let seconds = 0;
+// let minutes = 0;
+// let index = 0;
+// let timer = null; //2 часа для понимания этой фичи)
+//
+// function startTimer(){
+//     if (document.querySelector('.seconds').value == null || document.querySelector('.minutes').value == null){
+//         document.querySelector('.seconds').value = 1;
+//         document.querySelector('.minutes').value = 1;
+//     } else if (document.querySelector('.seconds').value < 0 || document.querySelector('.minutes').value < 0){
+//         console.log("Отрицательные значения");
+//         return 0;
+//     }
+//
+//     seconds = +document.querySelector('.seconds').value;
+//     minutes = +document.querySelector('.minutes').value;
+//     index = -1;
+//     timer = setInterval(updateTime, 1000);
+// }
+//
+// function updateTime(){
+//     if (seconds == 0 && minutes == 0 && index == 1){
+//         clearInterval(timer);
+//         console.log("Закройся")
+//         return;
+//     }
+//
+//     if (seconds == 0 && minutes != 0){
+//         minutes -= 1;
+//         seconds = 60;
+//         document.querySelector('.minutes').value = minutes;
+//         document.querySelector('.seconds').value = seconds;
+//     }
+//
+//     seconds -= 1;
+//     index = 1;
+//     document.querySelector('.seconds').value = seconds;
+// }
