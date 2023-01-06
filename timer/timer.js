3f644ba1803b383408687195e66b6e94549f763d
Андрей Барышев <baryshev_2002@inbox.ru>
2023-01-06 20:43:13 +0600

    почистил ворнинги

--- timer/timer.js
+++ timer/timer.js
@@ -1,7 +1,7 @@
 /* Значение таймера 0:0 */
 /* Объявил интервал */
 let timer = null;
-let sound = new Audio(`../sound/sound.mp3`);
+let sound = new Audio(`sound/sound.mp3`);
 
 /* Первая загрузка */
 if
@@ -9,40 +9,40 @@ if
     localStorage.setItem('minuteId', JSON.stringify(0));
     localStorage.setItem('secondId', JSON.stringify(0));
     localStorage.setItem('condition', JSON.stringify(0));
-    localStorage.setItem('stopedTimer', JSON.stringify(0));
+    localStorage.setItem('stoppedTimer', JSON.stringify(0));
     localStorage.setItem('beginTimer', JSON.stringify(0));
     console.log("Первый запуск таймера: грузим локальное хранилище")
 }
 
-if (JSON.parse(localStorage.getItem('condition')) == 1 && JSON.parse(localStorage.getItem('stopedTimer')) == 0){
+if (JSON.parse(localStorage.getItem('condition')) === 1 && JSON.parse(localStorage.getItem('stoppedTimer')) === 0){
     timer = setInterval(updateTimer, 1000);
 }
 
 function updateTimer(){
-    if (JSON.parse(localStorage.getItem('beginTimer')) == 0 && JSON.parse(localStorage.getItem('secondId')) <= 0 && JSON.parse(localStorage.getItem('minuteId')) <= 0){
+    if (JSON.parse(localStorage.getItem('beginTimer')) === 0 && JSON.parse(localStorage.getItem('secondId')) <= 0 && JSON.parse(localStorage.getItem('minuteId')) <= 0){
         clearInterval(timer);
         timer = null;
         localStorage.setItem('condition', JSON.stringify(0));
         localStorage.setItem('beginTimer', JSON.stringify(0));
         return;
     }
-    if (JSON.parse(localStorage.getItem('secondId')) <= 0 && JSON.parse(localStorage.getItem('minuteId')) <= 0 && JSON.parse(localStorage.getItem('condition')) == 1){
+    if (JSON.parse(localStorage.getItem('secondId')) <= 0 && JSON.parse(localStorage.getItem('minuteId')) <= 0 && JSON.parse(localStorage.getItem('condition')) === 1){
         clearInterval(timer);
         timer = null;
         localStorage.setItem('condition', JSON.stringify(0));
         document.body.style.backgroundColor = "#ff0000"
-
-        return sound.play();
+        sound.play();
+        return;
         //console.log("Не конец?")
-    };
+    }
 
-    if (JSON.parse(localStorage.getItem('secondId')) == 0 && JSON.parse(localStorage.getItem('condition')) == 1){
-        localStorage.setItem('minuteId', JSON.stringify(JSON.parse(localStorage.getItem('minuteId'))) - 1);
+    if (JSON.parse(localStorage.getItem('secondId')) === 0 && JSON.parse(localStorage.getItem('condition')) === 1){
+        localStorage.setItem('minuteId', JSON.stringify(JSON.parse(localStorage.getItem('minuteId')) - 1));
         console.log("Прошла минута")
         localStorage.setItem('secondId', JSON.stringify(60));
-    };
-    if (JSON.parse(localStorage.getItem('condition')) == 1){
-        if (JSON.parse(localStorage.getItem('beginTimer')) != 1){
+    }
+    if (JSON.parse(localStorage.getItem('condition')) === 1){
+        if (JSON.parse(localStorage.getItem('beginTimer')) !== 1){
             localStorage.setItem('beginTimer', JSON.stringify(1));
         }
         console.log("Прошла секунда");
@@ -50,14 +50,14 @@ function updateTimer(){
         document.querySelector('.minutes').value = JSON.parse(localStorage.getItem('minuteId'));
         document.querySelector('.seconds').value = JSON.stringify(JSON.parse(JSON.parse(localStorage.getItem('secondId'))));
     }
-    if (JSON.parse(localStorage.getItem('condition')) == 0){
-        clearInterval();
+    if (JSON.parse(localStorage.getItem('condition')) === 0){
+        clearInterval(timer);
     }
 }
 
 /* Кнопки */
 function startTimer(){
-    if (timer != null || JSON.parse(localStorage.getItem('stopedTimer')) == 1){
+    if (timer != null || JSON.parse(localStorage.getItem('stoppedTimer')) === 1){
         return;
     }
 
@@ -65,20 +65,20 @@ function startTimer(){
     localStorage.setItem('minuteId', document.querySelector('.minutes').value);
     localStorage.setItem('secondId', document.querySelector('.seconds').value);
     localStorage.setItem('condition', JSON.stringify(1));
-    localStorage.setItem('stopedTimer', JSON.stringify(0));
+    localStorage.setItem('stoppedTimer', JSON.stringify(0));
 
     timer = setInterval(updateTimer, 1000);
 }
 
 function stopTimer(){
-    if (JSON.parse(localStorage.getItem('condition') == 1)){
+    if (JSON.parse(localStorage.getItem('condition')) === 1){
         console.log('stop');
-        if (JSON.parse(localStorage.getItem('stopedTimer')) == 0){
+        if (JSON.parse(localStorage.getItem('stoppedTimer')) === 0){
             clearInterval(timer);
-            localStorage.setItem('stopedTimer', JSON.stringify(1));
+            localStorage.setItem('stoppedTimer', JSON.stringify(1));
         } else{
             timer = setInterval(updateTimer, 1000);
-            localStorage.setItem('stopedTimer', JSON.stringify(0));
+            localStorage.setItem('stoppedTimer', JSON.stringify(0));
         }
     }
 }
@@ -94,12 +94,12 @@ function resetTimer(){
     localStorage.setItem('minuteId', JSON.stringify(0));
     localStorage.setItem('secondId', JSON.stringify(0));
     localStorage.setItem('condition', JSON.stringify(0));
-    localStorage.setItem('stopedTimer', JSON.stringify(0));
+    localStorage.setItem('stoppedTimer', JSON.stringify(0));
     localStorage.setItem('beginTimer', JSON.stringify(0));
 }
 
 function oneMinute(){
-    if (timer != null || JSON.parse(localStorage.getItem('stopedTimer')) == 1){
+    if (timer != null || JSON.parse(localStorage.getItem('stoppedTimer')) === 1){
         return;
     }
     console.log("1 минута");
@@ -108,7 +108,7 @@ function oneMinute(){
 }
 
 function fiveMinute(){
-    if (timer != null || JSON.parse(localStorage.getItem('stopedTimer')) == 1){
+    if (timer != null || JSON.parse(localStorage.getItem('stoppedTimer')) === 1){
         return;
     }
     console.log("5 минута");
@@ -117,7 +117,7 @@ function fiveMinute(){
 }
 
 function tenMinutes(){
-    if (timer != null || JSON.parse(localStorage.getItem('stopedTimer')) == 1){
+    if (timer != null || JSON.parse(localStorage.getItem('stoppedTimer')) === 1){
         return;
     }
     console.log("10 минута");
