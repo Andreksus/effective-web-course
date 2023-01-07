c6acd7f96bea4134cd981be84d424cba278ec4d0
Андрей Барышев <baryshev_2002@inbox.ru>
2023-01-07 18:44:22 +0600

    finally initial commit of a working app

--- third_lab/vite.config.ts
+++ third_lab/vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import eslintPlugin from 'vite-plugin-eslint';
-import checker from 'vite-plugin-checker';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteTsconfigPaths(),
-    eslintPlugin(),
-    checker({ typescript: true })
-  ],
-  server: {
-    open: true
-  }
-});
+  plugins: [react()],
+})
