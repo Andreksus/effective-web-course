fd76460bcef2000a757ad42bed46a9162827ea1d
Андрей Барышев <baryshev_2002@inbox.ru>
2023-01-08 18:42:29 +0600

    сделал header, применив styled components framework

---
+++ third_lab/src/components/Header.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+export function Header(){
+    return(
+        <Head>
+            <Logo>
+                <img src="marvel_logo.svg" alt=""/>
+            </Logo>
+            <Links>
+                <Links_Pages><A cur={1} href="#">Characters</A></Links_Pages>
+                <Links_Pages><A cur={0} href="#">Comics</A></Links_Pages>
+                <Links_Pages><A cur={0} href="#">Series</A></Links_Pages>
+            </Links>
+        </Head>
+    )
+}
+
+const Head = styled.div`
+  background: #ed1a3b;
+  display: flex;
+  flex-direction: row;
+`
+
+const Logo = styled.div`
+  padding-top: 20px;
+  padding-left: 15px;
+  height: 60px;
+  width: 100px;
+`
+
+const Img = styled.img`
+  height: 100%;
+  width: 100%;
+`
+
+const Links = styled.nav`
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 77%;
+`
+
+const Links_Pages = styled.li`
+  padding: 0 0 0 15px;
+  font-family: Avenir,serif;
+`
+
+type Props = {
+    cur: number;
+}
+
+const A = styled.a<Props>`
+  color: #f9c92d;
+  font-size: 22px;
+  text-decoration: ${props => props.cur === 1 ? 'underline' : 'none'};
+  font-weight: bold;
+
+`
