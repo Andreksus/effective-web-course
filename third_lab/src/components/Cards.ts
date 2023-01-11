a7d7cb7abdc1fa69087fcccef3e43011b0a4c987
Андрей Барышев <baryshev_2002@inbox.ru>
2023-01-11 19:33:49 +0600

    добавил в интерфейс icard refs для хранения ссылок карточки. Заполнил первую карточку.

--- third_lab/src/components/Cards.ts
+++ third_lab/src/components/Cards.ts
@@ -8,6 +8,7 @@ export const characters: ICard[] = [
         title: "AKHENATEN",
         description: "Pharaoh during Egypt's 18th Dynasty, Amenhotep established a monotheistic religion worshipping the sun god Aten, alleged creator of the universe.",
         image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/akhenaten.jpg",
+        refs: [["https://drawnstories.ru/comics/marvel-comics/Thanos/marvel-universe-the-end", ["Marvel Universe the End #001", "Pharaoh vs. the Marvel universe #002", "To ancient Egypt - on a time machine! #003"]],["#",["there are no TV series or movies with these characters yet..", ""]]]
     },
 
     {
@@ -16,6 +17,7 @@ export const characters: ICard[] = [
         title: "SCARLET SPIDER",
         description: "Ben Reilly was the most genetically stable of the many clones of Spider-Man created by Professor Miles Warren, the Jackal.",
         image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/reilly_head.jpg",
+        refs: [["https://drawnstories.ru/comics/marvel-comics/Thanos/marvel-universe-the-end", ["Marvel Universe the End #001", "Pharaoh vs. the Marvel universe #002", "To ancient Egypt - on a time machine! #003"]],["#",["there are no TV series or movies with these characters yet..", ""]]]
     },
 
     {
@@ -24,6 +26,7 @@ export const characters: ICard[] = [
         title: "GHOST RIDER",
         description: "Bonded with the demon Zarathos, the motorcycle stunt man Johnny Blaze becomes the feared Ghost Rider.",
         image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/125grb_com_crd_01.jpg",
+        refs: ([["https://drawnstories.ru/comics/marvel-comics/Thanos/marvel-universe-the-end", ["Marvel Universe the End #001", "Pharaoh vs. the Marvel universe #002", "To ancient Egypt - on a time machine! #003"]]])
     },
 
     {
@@ -32,6 +35,7 @@ export const characters: ICard[] = [
         title: "DOCTOR DOOM",
         description: "Victor Von Doom overcame his difficult childhood to become one of the world’s most brilliant scientists—though.",
         image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/057drd_com_crd_01.jpg",
+        refs: ([["https://drawnstories.ru/comics/marvel-comics/Thanos/marvel-universe-the-end", ["Marvel Universe the End #001", "Pharaoh vs. the Marvel universe #002", "To ancient Egypt - on a time machine! #003"]]])
     }
 ]
 
@@ -42,6 +46,7 @@ export const comics: ICard[] = [
         title: "Fantastic Four (2022) #3",
         description: "The Human Torch is alone in New York, trying to survive in a city that hates and fears — well, mostly him specifically.",
         image: "https://i.annihil.us/u/prod/marvel/i/mg/3/b0/63b4782a80f83/portrait_uncanny.jpg",
+        refs: ([["https://drawnstories.ru/comics/marvel-comics/Thanos/marvel-universe-the-end", ["Marvel Universe the End #001", "Pharaoh vs. the Marvel universe #002", "To ancient Egypt - on a time machine! #003"]]])
     },
 
     {
@@ -50,6 +55,7 @@ export const comics: ICard[] = [
         title: "Shang-Chi: Master Of The Ten Rings (2023) #1",
         description: "Shang-Chi is lost in time and the only one who can help him is…his father?!",
         image: "https://i.annihil.us/u/prod/marvel/i/mg/6/d0/63b477fe118c7/portrait_uncanny.jpg",
+        refs: ([["https://drawnstories.ru/comics/marvel-comics/Thanos/marvel-universe-the-end", ["Marvel Universe the End #001", "Pharaoh vs. the Marvel universe #002", "To ancient Egypt - on a time machine! #003"]]])
     },
 
     {
@@ -58,6 +64,7 @@ export const comics: ICard[] = [
         title: "Spider-Man (2022) #4",
         description: "The End of the Spider-Verse rages on! The tremors will be felt until it all ends!",
         image: "https://i.annihil.us/u/prod/marvel/i/mg/7/00/63b477fd894a2/portrait_uncanny.jpg",
+        refs: ([["https://drawnstories.ru/comics/marvel-comics/Thanos/marvel-universe-the-end", ["Marvel Universe the End #001", "Pharaoh vs. the Marvel universe #002", "To ancient Egypt - on a time machine! #003"]]])
     }
 ]
 
@@ -68,6 +75,7 @@ export const series: ICard[] = [
         title: "Spider-Man: No Way Home",
         description: "After Quentin Beck frames Peter Parker for his murder and reveals Parker's identity as Spider-Man.",
         image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/spider-mannowayhome_lob_crd_03.jpg",
+        refs: ([["https://drawnstories.ru/comics/marvel-comics/Thanos/marvel-universe-the-end", ["Marvel Universe the End #001", "Pharaoh vs. the Marvel universe #002", "To ancient Egypt - on a time machine! #003"]]])
     },
 
     {
@@ -76,6 +84,7 @@ export const series: ICard[] = [
         title: "Avengers: EndGame",
         description: "In 2018, twenty-three days after Thanos erased half of all life in the universe,Carol Danvers rescues Tony Stark and Nebula from deep gal..",
         image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05_2.jpg",
+        refs: ([["https://drawnstories.ru/comics/marvel-comics/Thanos/marvel-universe-the-end", ["Marvel Universe the End #001", "Pharaoh vs. the Marvel universe #002", "To ancient Egypt - on a time machine! #003"]]])
     },
 
     {
@@ -84,6 +93,7 @@ export const series: ICard[] = [
         title: "Ant Man and The Wasp: Quantumania",
         description: "Super-Hero partners Scott Lang (Paul Rudd) and Hope Van Dyne (Evangeline Lilly) return to continue their..",
         image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/antmanandthewaspquantumania_lob_crd_02.jpg",
+        refs: ([["https://drawnstories.ru/comics/marvel-comics/Thanos/marvel-universe-the-end", ["Marvel Universe the End #001", "Pharaoh vs. the Marvel universe #002", "To ancient Egypt - on a time machine! #003"]]])
     },
 
     {
@@ -92,5 +102,6 @@ export const series: ICard[] = [
         title: "Doctor Strange in the Multiverse of Madness",
         description: "America Chavez and a version of Stephen Strange are chased by a demon in the space between..",
         image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/doctorstrangeinthemultiverseofmadness_lob_crd_02_3.jpg",
+        refs: ([["https://drawnstories.ru/comics/marvel-comics/Thanos/marvel-universe-the-end", ["Marvel Universe the End #001", "Pharaoh vs. the Marvel universe #002", "To ancient Egypt - on a time machine! #003"]]])
     }
 ]
--- third_lab/src/types/ICard.ts
+++ third_lab/src/types/ICard.ts
@@ -4,4 +4,6 @@ export interface ICard{
     title: string
     description: string
     image: string
+    //refs: [string, string]
+    refs: [string, string[]][] //массив ссылок, где каждой ссылке соответствует массив комиксов
 }
